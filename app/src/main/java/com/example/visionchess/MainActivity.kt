1b61239a9eef94fe970e223a4e1e5f509ae47263
WiktorPawlak17 <pawlak.wiktor00@gmail.com>
2023-07-10 10:38:01 +0200

    Version 0.0.1

--- app/src/main/java/com/example/visionchess/MainActivity.kt
+++ app/src/main/java/com/example/visionchess/MainActivity.kt
@@ -5,29 +5,50 @@ import android.os.Handler
 import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
+    private val handler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
 
             Box(
                 contentAlignment = Alignment.Center,
-                modifier = Modifier.fillMaxSize().background(Color.Black)
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black)
             ) {
+                Image(
+                    painter = painterResource(id = R.drawable.actuallogobig),
+                    contentDescription = "Vision Chess Logo",
+                    modifier = Modifier.offset(y = (-100).dp)
+                )
                 ProgressBar(percentage = 1f, number = 100)
+
             }
         }
-        Handler(Looper.getMainLooper()).postDelayed({
+        handler.postDelayed({
             setContentView(R.layout.activity_main)
         }, 3000)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
+    }
+
+
 }
 
