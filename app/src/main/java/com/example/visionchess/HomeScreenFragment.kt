f2743aa3446f92c1c276f29355da1e0261e824ce
WiktorPawlak17 <pawlak.wiktor00@gmail.com>
2023-07-25 16:30:10 +0200

    Version 0.0.4.2 There is a not working menu, higher, speeds up really nice

--- app/src/main/java/com/example/visionchess/HomeScreenFragment.kt
+++ app/src/main/java/com/example/visionchess/HomeScreenFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,25 +19,37 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeScreenFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_home_screen, container, false)
+        val menucirclewithbuttons = rootView.findViewById<ImageView>(R.id.menucirclewithbuttons)
+        val animationFadeIn = AnimationUtils.loadAnimation(context, R.anim.fade_in_quick)
+        menucirclewithbuttons.startAnimation(animationFadeIn)
+
+        /*
+        //This is here for me to have the code for the future
+        val handler = android.os.Handler(android.os.Looper.getMainLooper())
+        handler.postDelayed({
+            val animationFadeIn = AnimationUtils.loadAnimation(context, R.anim.fade_in_very_quick)
+            menucirclewithbuttons.startAnimation(animationFadeIn)
+        }, 0)
+         */
+        return rootView
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val handler = android.os.Handler(android.os.Looper.getMainLooper())
+        handler.removeCallbacksAndMessages(null)
+    }
+
 
     companion object {
         /**
