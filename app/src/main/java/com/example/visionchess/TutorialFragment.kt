777c152c80271d94554af4d1084bd971905225df
WiktorPawlak17 <pawlak.wiktor00@gmail.com>
2023-07-31 10:00:09 +0200

    Version 0.0.6.1 The Menu and tutorial trying to progress

--- app/src/main/java/com/example/visionchess/TutorialFragment.kt
+++ app/src/main/java/com/example/visionchess/TutorialFragment.kt
@@ -33,6 +33,7 @@ class TutorialFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tutorial, container, false)
     }
